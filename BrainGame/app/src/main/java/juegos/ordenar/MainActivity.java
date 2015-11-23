8b251198c88c0378eb593ce5a26fb6e89dd4de2c
EvaRo <eva.roddeck@rub.de>
2015-11-23 16:24:23 +0100

    Github Test Eva
    -

--- BrainGame/app/src/main/java/juegos/ordenar/MainActivity.java
+++ BrainGame/app/src/main/java/juegos/ordenar/MainActivity.java
@@ -15,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.ordenar_imagenes_activity_main);
+
+        //Github test 
     }
 }
